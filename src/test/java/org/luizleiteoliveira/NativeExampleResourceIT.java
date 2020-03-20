c56471b976c1a0467caef045cd2ccc5668c6edff
luizleite-hotmart <luiz.leite@hotmart.com>
2020-03-20 08:02:02 -0300

    create project clean with quarkus

---
+++ src/test/java/org/luizleiteoliveira/NativeExampleResourceIT.java
@@ -0,0 +1,9 @@
+package org.luizleiteoliveira;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeExampleResourceIT extends ExampleResourceTest {
+
+    // Execute the same tests but in native mode.
+}
